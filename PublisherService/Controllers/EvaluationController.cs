88a8b0885d1c403d9b12517349e9abf3732b2f27
Johannes Schumeth <johannes.schumeth@gmail.com>
2022-02-22 16:07:26 +0100

    cleanup project

---
+++ PublisherService/Controllers/EvaluationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PublisherService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EvaluationController : ControllerBase
+    {
+        private readonly ILogger<EvaluationController> logger;
+
+        public EvaluationController(ILogger<EvaluationController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task EvaluateMessageBroker(int numberOfMessages)
+        {
+            logger.LogInformation("Received Request");
+        }
+    }
+}
