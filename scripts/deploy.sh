1435ef549c5eefa9840e257938de2107259a94b0
Johannes Schumeth <johannes.schumeth@gmail.com>
2022-02-24 19:24:20 +0100

    use realpath in scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-docker-compose up -d --build
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+ROOT_DIR=${SCRIPT_DIR}/../
+
+docker-compose -f ${ROOT_DIR}/docker-compose.yaml up -d --build
 
 echo ""
 echo "[*] Done"
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-docker-compose down
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+ROOT_DIR=${SCRIPT_DIR}/../
+
+docker-compose -f ${ROOT_DIR}/docker-compose.yaml down
 
 echo ""
 echo "[*] Done"
